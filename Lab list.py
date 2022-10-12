255446a3476856a8313f378f216d48bbfdb52dcd
saja <s.aldossary@hotmail.com>
2022-10-12 21:47:28 +0300

    LIST.PY

---
+++ Lab list.py
@@ -0,0 +1,20 @@
+num_list = [2, 3, 4, 5, 15, 1, 43, 20]
+sum = sum (num_list)
+print(sum)
+
+largest = max (num_list)
+print(largest)
+               
+num_list= [*range(1200, 2000, 125)]
+odd_list=[]
+for i in num_list:
+    if i % 2 != 0:
+        odd_list.append(i)
+print(odd_list)
+
+new_list = num_list[:5]
+
+print(new_list)
+
+
+    
