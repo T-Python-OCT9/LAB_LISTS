760047a18e61acf8c7ac7e4ae51804f3dd2cc3bf
Razan Alshehri <alshehri.razan8@gmail.com>
2022-10-12 20:34:37 +0300

    Lists Lab

---
+++ LISTS.py
@@ -0,0 +1,16 @@
+from cgi import print_arguments
+
+
+new_list = [2,3,4,5,15,1,43,20]
+print ("the sum of the list elemnts is :", sum(new_list))
+print ("the largest number of the list elemnts is :", max (new_list))
+
+
+my_list = range (1200, 2000, 125)
+my_list = [x for x in my_list]
+print (my_list)
+
+
+print (my_list [1 : 6])
+
+
